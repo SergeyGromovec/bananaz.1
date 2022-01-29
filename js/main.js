40eac325a7528cda5148fc8a628a06a148e19d94
Sergey <90825413+SergeyGromovec@users.noreply.github.com>
2022-01-29 18:49:15 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,49 @@
+const mobileNavButton = document.querySelector('.mobile-nav-button');
+const mobileNavIcon = document.querySelector('.mobile-nav-button__icon');
+const mobileNav = document.querySelector('.mobile-nav');
+
+mobileNavButton.addEventListener('click', function () {
+    mobileNavIcon.classList.toggle('active');
+    mobileNav.classList.toggle('active');
+    document.body.classList.toggle('no-scroll');
+})
+
+// video-nav
+const videoBtn = document.querySelector('#video-story-btn');
+const videoFile = document.querySelector('#video-story');
+const videoOverlay = document.querySelector('#video-story-overlay');
+const videoBtnIcon = document.querySelector('#video-story-btn-icon');
+
+videoBtn.addEventListener('click', function () {
+
+
+
+    function toggleOverlay(event) {
+        if (event.type === 'mouseleave') {
+            videoOverlay.classList.add('hidden');
+        } else {
+            videoOverlay.classList.remove('hidden');
+        }
+
+    }
+
+    if (videoFile.paused) {
+        videoFile.play();
+        videoBtnIcon.src = "/img/story/pause.svg";
+
+        videoOverlay.onmouseleave = toggleOverlay;
+        videoOverlay.onmouseenter = toggleOverlay;
+
+    } else {
+        videoFile.pause();
+        videoBtnIcon.src = "/img/story/play-white.svg";
+        videoOverlay.onmouseleave = null;
+        videoOverlay.onmouseenter = null;
+    }
+
+
+
+
+
+})
+
